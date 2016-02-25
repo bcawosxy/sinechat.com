10cba8a4f7cb187626b4a3a56806e561c02a83ae
Mars-Dev <bcawosxy@gmail.com>
2016-02-25 15:20:30 +0800

    v0.1.3
    1.分層完成(前後台)

--- config/global.php
+++ config/global.php
@@ -3,37 +3,32 @@
  * PATH
  */
 define('PATH_ROOT', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
-define('PATH_CONFIG', PATH_ROOT.'config'.DIRECTORY_SEPARATOR);
-define('PATH_CACHE', PATH_ROOT.'cache'.DIRECTORY_SEPARATOR);
-define('PATH_LANG', PATH_ROOT.'lang'.DIRECTORY_SEPARATOR);
-define('PATH_LIB', PATH_ROOT.'lib'.DIRECTORY_SEPARATOR);
-define('PATH_LOG', PATH_ROOT.'log'.DIRECTORY_SEPARATOR);
-define('PATH_MODEL', PATH_ROOT.'model'.DIRECTORY_SEPARATOR);
-define('PATH_MODULE', PATH_ROOT.'module'.DIRECTORY_SEPARATOR);
-define('PATH_SDK', PATH_ROOT.'sdk'.DIRECTORY_SEPARATOR);
-define('PATH_STATIC_FILE', PATH_ROOT.'static_file'.DIRECTORY_SEPARATOR);
-define('PATH_STORAGE', PATH_ROOT.'storage'.DIRECTORY_SEPARATOR);
-define('PATH_TTF', PATH_ROOT.'ttf'.DIRECTORY_SEPARATOR);
-define('PATH_UPLOAD', PATH_ROOT.'upload'.DIRECTORY_SEPARATOR);
+define('PATH_CSS', PATH_ROOT.'assets'.DIRECTORY_SEPARATOR.'css'.DIRECTORY_SEPARATOR);
+// define('PATH_CACHE', PATH_ROOT.'cache'.DIRECTORY_SEPARATOR);
+// define('PATH_LANG', PATH_ROOT.'lang'.DIRECTORY_SEPARATOR);
+// define('PATH_LIB', PATH_ROOT.'lib'.DIRECTORY_SEPARATOR);
+// define('PATH_LOG', PATH_ROOT.'log'.DIRECTORY_SEPARATOR);
+// define('PATH_MODEL', PATH_ROOT.'model'.DIRECTORY_SEPARATOR);
+// define('PATH_MODULE', PATH_ROOT.'module'.DIRECTORY_SEPARATOR);
+// define('PATH_SDK', PATH_ROOT.'sdk'.DIRECTORY_SEPARATOR);
+// define('PATH_STATIC_FILE', PATH_ROOT.'static_file'.DIRECTORY_SEPARATOR);
+// define('PATH_STORAGE', PATH_ROOT.'storage'.DIRECTORY_SEPARATOR);
+// define('PATH_TTF', PATH_ROOT.'ttf'.DIRECTORY_SEPARATOR);
+// define('PATH_UPLOAD', PATH_ROOT.'upload'.DIRECTORY_SEPARATOR);
 
 /**
  * URL
  */
-define('URL_CSS', URL_ROOT.'css/'.SITE_LANG.'/');
-define('URL_IMG', URL_ROOT.'img/'.SITE_LANG.'/');
-define('URL_JS', URL_ROOT.'js/');
-define('URL_STATIC_FILE', URL_ROOT.'static_file/');
-define('URL_STORAGE', URL_ROOT.'storage/');
-define('URL_UPLOAD', URL_ROOT.'upload/');
+define('URL_CSS', 'http://'.URL_ROOT.'/assets/css/');
+define('URL_IMG', 'http://'.URL_ROOT.'/assets/images/');
+define('URL_JS', 'http://'.URL_ROOT.'/assets/js/');
+define('URL_UPLOAD', 'http://'.URL_ROOT.'upload/');
+
+
 
 /**
- * X_site
+ * 引入[view][controller]判斷
  */
-//news.act
-$CONFIG['NEWS_ACT'] = array('close'=>_('Close'), 'preview'=>_('Preview'), 'open'=>_('Open'));
+(!is_dir( PATH_ROOT.'view'.DIRECTORY_SEPARATOR.SITE_NAME.DIRECTORY_SEPARATOR._CLASS)) ? redirect('http://'.URL_ROOT) : null;
+(!is_file( PATH_ROOT.'controller'.DIRECTORY_SEPARATOR._CLASS.'.php')) ?  : include(PATH_ROOT.'controller'.DIRECTORY_SEPARATOR._CLASS.'.php');
 
-//news.class (目前 magazine 有獨立的顯示頁面)
-$CONFIG['NEWS_CLASS'] = array('blog'=>_('婚享名人'), 'company'=>_('婚享好友'), 'event'=>_('婚享優惠'), 'fan'=>_('婚享報報'), 'lectures'=>_('婚享講座'), 'magazine'=>_('婚享誌'));
-
-//newsarea.act
-$CONFIG['NEWSAREA_ACT'] = array('close'=>_('Close'), 'preview'=>_('Preview'), 'open'=>_('Open'));
--- config/sinechat.php
+++ config/sinechat.php
@@ -1,6 +1,6 @@
 <?php 
 	date_default_timezone_set('Asia/Taipei');
-	
+	define('SITE_NAME', 'sinechat');
 	//route
 	function route_rule($url) {
 		$package = null;
@@ -11,24 +11,46 @@
 		$level1 = empty($a_url[1])? 'index' : $a_url[1];
 		$level2 = empty($a_url[2])? 'index' : $a_url[2];
 		$level3 = empty($a_url[3])? 'index' : $a_url[3];
-		$level4 = empty($a_url[4])? 'index' : $a_url[4];
 		switch ($level1) {
 			case 'admin':
+			
+				define('MAIN', 'admin');
 			break;
 			
 			default :
 				$package = $level1;
+				define('MAIN', SITE_NAME);
 			break;
 		}
 		
 		return array($package, $class, $function, $version);
 	}
+	
 	define('SITE_FOLDER', null);
+	$dev_root = $_SERVER['SERVER_NAME'];
 	$SITE_FOLDER = constant('SITE_FOLDER');
 	$pos = strpos($_SERVER['REQUEST_URI'], $SITE_FOLDER);
 	list($url) = explode('?', ($pos !== false)? substr_replace($_SERVER['REQUEST_URI'], '', $pos, strlen($SITE_FOLDER)) : $_SERVER['REQUEST_URI']);
 	list($package, $class, $function, $version) = route_rule($url);
 	define('_CLASS', $package);
+	define('URL_ROOT', $dev_root);	
 	
-	//不同環境的DB 設定預計在這邊處理
+	//不同環境的DB 設定
+	switch($dev_root) {
+		/**
+		 *	開發環境
+		 */
+		case 'dev.sinechat.com':
+			
+		break;
+		
+		/**
+		 *	正式環境環境
+		 */
+		case 'www.sinechat.com':
+		case 'sinechat.com':
+		
+		break;
+		
+	}
 ?>
