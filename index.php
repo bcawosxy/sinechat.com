e5c74cb2d371ef017b99667a3446d9d0154156c3
Mars-Home <bcawosxy@gmail.com>
2016-04-11 22:12:57 +0800

    v3.7
    1.加入GA分析

--- index.php
+++ index.php
@@ -34,6 +34,18 @@ include('./config/global.php');
 				<meta property="og:site_name" content="新誠修繕工程" />
 			</head>
 			<body class="homepage">
+				<?php 
+					if(ENV == 'production') {
+						echo '<script>
+							(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+							(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+							m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+							})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
+							ga(\'create\', \'UA-76261914-1\', \'auto\');
+							ga(\'send\', \'pageview\');
+						</script>';
+					}
+				?>
 				<div id="page-wrapper">
 					<!-- Headbar --><?php include('./view'.DIRECTORY_SEPARATOR.SITE_NAME.DIRECTORY_SEPARATOR.'headbar.phtml') ?> <!-- end Headbar -->
 					<!-- Banner --> <?php include('./view'.DIRECTORY_SEPARATOR.SITE_NAME.DIRECTORY_SEPARATOR.'banner.phtml') ?> <!-- end Banner -->		
