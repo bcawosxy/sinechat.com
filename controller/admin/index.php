6242da491781edec6125811dfc6e76d604f8ab9a
Mars-Dev <bcawosxy@gmail.com>
2016-06-07 18:38:01 +0800

    v3.13
    1.後台圖表樣式調整

--- controller/admin/index.php
+++ controller/admin/index.php
@@ -1,4 +1,23 @@
 <?php 
+//本周最後一天(星期日)
+$this_week = date('Y-m-d', strtotime('+7 day', time()-86400*date('w')));
+$series_line = []; $rate = 7;
+for($i=1; $i<8; $i = $i+1) {$tmp[] = date('Y-m-d 23:59:59', strtotime('-'.($i*$rate).' day', strtotime($this_week)));}
+array_unshift($tmp,$this_week);
+$week = array_reverse($tmp);
+$bar_data = [];
+foreach ($week as $k0 => $v0) {
+	if($k0 == 0) continue;
+	$param = ['status'=>'delete'];
+	$where = [[[['product.status', '!=', ':status' ], ['TO_DAYS(`product`.`inserttime`)', '>', 'TO_DAYS("'.$week[$k0-1].'")'], ['TO_DAYS(`product`.`inserttime`)', '<', 'TO_DAYS("'.$v0.'")']], 'and']];
+	$m_product = Model('product')->column(['COUNT(1) as count'])->where($where)->param($param)->fetch();
+
+	$bar_data[] = [	
+		'data' => $m_product['count'],
+		'date' => $v0,
+	];
+}
+
 $column = ['service.*','COUNT(1) as num'];
 $join =[['left join', 'service', 'using(`service_id`)']];
 $param = ['status'=>'open'];
